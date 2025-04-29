05e1434f12f0e1cd05f5401ab0c23a0fb43a292e
SamKerubin <162046796+SamKerubin@users.noreply.github.com>
2025-04-28 23:52:13 -0500

    plugin icon

--- addons/godot graph visualizer/graph_visualizer.gd
+++ addons/godot graph visualizer/graph_visualizer.gd
@@ -2,7 +2,7 @@
 extends EditorPlugin
 
 const MAIN_PANEL: PackedScene = preload("uid://c4h2pps361nfk")
-const ICON: Texture2D = preload("res://icon.svg")
+const ICON: Texture2D = preload("res://icon.png")
 const PLUGIN_NAME: String = "Visualize Project"
 
 var main_panel_instance : Main
