4ed5b32b7f73f00952a798a057e94f1100b8372b
SamKerubin <sammm.1618033@gmail.com>
2025-11-17 21:14:25 -0500

    [fixed] bug in scene property reference fixed
    now the graph displays correctly the scene nodes referencing scene instances

--- addons/godot graph visualizer/project_scanners/scene_scanner/scene_property_manager.gd
+++ addons/godot graph visualizer/project_scanners/scene_scanner/scene_property_manager.gd
@@ -18,29 +18,24 @@ func _init() -> void:
 
 	_script_parser_manager = ScriptParserManager.new()
 
+#region Resource getters
 func _open_scene(path: String) -> Node:
 	var scn: PackedScene = ResourceLoader.load(path, "PackedScene")
 	return scn.instantiate()
 
-#region Scene Matching
-## Given a [param path], opens the scene file
-## and search for each direct instance (child)
-## the scene have
-func _check_scene(path: String) -> void:
-	if find_scene_with_path(path):
-		return
+## Given a living array of nodes [param children][br]
+## Returns a serialized copy of those children
+func _copy_children(children: Array[Node]) -> Array[Dictionary]:
+	var result: Array[Dictionary] = []
 
-	if not ResourceLoader.exists(path):
-		push_error("Error: Invalid path \'%s\'" % path)
-		return
+	for c: Node in children:
+		result.append({
+			"name": c.name,
+			"scene_file_path": c.scene_file_path,
+			"children": _copy_children(c.get_children())
+		}) 
 
-	var scn_root: Node = _open_scene(path)
-
-	var scene_data: SceneData = _search_in_scene(scn_root, path)
-	_scene_properties.append(scene_data)
-
-	_search_instances(scene_data, scn_root.get_children())
-	scn_root.free()
+	return result
 
 ## Using a scene [param scn] and its [param path],
 ## returns a [class SceneData] instance
@@ -56,6 +51,28 @@ func _search_in_scene(scn: Node, path: String) -> SceneData:
 	scene_data.get_properties().set_editor_description(scn.editor_description)
 
 	return scene_data
+#endregion
+
+#region Scene Matching
+## Given a [param path], opens the scene file
+## and search for each direct instance (child)
+## the scene have
+func _check_scene(path: String) -> void:
+	if find_scene_with_path(path):
+		return
+
+	if not ResourceLoader.exists(path):
+		push_error("Error: Invalid path \'%s\'" % path)
+		return
+
+	var scn_root: Node = _open_scene(path)
+	var children: Array[Dictionary] = _copy_children(scn_root.get_children())
+
+	var scene_data: SceneData = _search_in_scene(scn_root, path)
+	scn_root.free()
+	_scene_properties.append(scene_data)
+
+	_search_instances(scene_data, children)
 
 ## Search the attached script a [param scn] would have[br]
 ## If the scene have a script, returns an instance of
@@ -76,13 +93,24 @@ func _search_attached_script(scn: Node) -> ScriptData:
 # find a way to check each child of a scene and add its script to the original scene
 # if it doesnt is a previously created instance
 
-func _filter_children(instance_children: Array[Node], 
-					own_children: Array[Node]) -> Array[Node]:
-	var instance_children_names: Array[String] = []
-	for child: Node in instance_children:
-		instance_children_names.append(child.name)
-
-	return own_children.filter(func(x: Node) -> bool: return x.name in instance_children_names)
+## When you open a scene as root, it may have different children as when you
+## instantiate it inside another scene [br]
+## [param instance_children] indicates the children a scene has while being instantiated
+## inside another scene[br]
+## [param own_children] indicates the children a scene has while being instantiated
+## as root[br]
+## It returns an array with every children exclusively from the instance, excluding nodes
+## declared by the parent node
+func _filter_children(instance_children: Array[Dictionary], 
+					own_children: Array[Dictionary]) -> Array[Dictionary]:
+	var instance_names: Array[String] = []
+	for child: Dictionary in instance_children:
+		instance_names.append(child["name"])
+	
+	return own_children.filter(
+		func(x: Dictionary) -> bool:
+			return not x["name"] in instance_names
+	)
 
 ## Handles an instance found inside a node[br]
 ## [param instance_path] is the path of the instance[br]
@@ -92,30 +120,29 @@ func _filter_children(instance_children: Array[Node],
 ## and only counting the ones declared within [param current_scene] scene
 func _handle_instance(instance_path: String, 
 					current_scene: SceneData,
-					child: Node) -> void:
+					child_children: Array[Dictionary]) -> void:
 	current_scene.get_properties().add_instance(instance_path)
 
 	var current_instance: Node = _open_scene(instance_path)
-	var child_nodes: Array[Node] = child.get_children()
-	var scene_nodes: Array[Node] = current_instance.get_children()
-	var new_children: Array[Node] = _filter_children(child_nodes, scene_nodes)
+	var instance_children: Array[Dictionary] = _copy_children(current_instance.get_children())
+	var root_children: Array[Dictionary] = _filter_children(instance_children, child_children)
 	current_instance.free()
-	_search_instances(current_scene, new_children)
+	_search_instances(current_scene, root_children)
 
 ## Uses a [param scn] scene and access to its children and
 ## calls recursively with each children of the node[br]
 ## For each instance, adds it to the current [param scene_data]
-func _search_instances(scene_data: SceneData, children: Array[Node]) -> void:
+func _search_instances(scene_data: SceneData, children: Array[Dictionary]) -> void:
 	if children.is_empty():
 		return
 
-	for child: Node in children:
-		var child_path: String = child.scene_file_path
+	for child: Dictionary in children:
+		var child_path: String = child["scene_file_path"]
 		if child_path != "":
-			_handle_instance(child_path, scene_data, child)
+			_handle_instance(child_path, scene_data, child["children"])
 			continue
 
-		_search_instances(scene_data, child.get_children())
+		_search_instances(scene_data, child["children"])
 
 ## Parses all scenes files in the project[br]
 ## Uses [param script_files] to initialize the script parser[br]
