c6b5efbe7df2cf4c0034803a846db426ddc1b620
SamKerubin <samuelkiller2013@gmail.com>
2025-12-03 16:04:31 -0500

    [bit refactor] adding skip method to the parser
    a method to skip tokens until <...> found
    added a method to parse return values
    made an enum wrapper

--- addons/godot-graph-visualizer/project_scanners/script_scanner/script_parser.gd
+++ addons/godot-graph-visualizer/project_scanners/script_scanner/script_parser.gd
@@ -2,8 +2,30 @@
 extends Resource
 class_name ScriptParser
 
+const _SYMBOLS := ScriptSymbolIndex.SymbolType
+
+func _skip_until(stop_tokens: Array[ScriptSymbolIndex.SymbolType],
+				tokens: Array[AST.Token],
+				current: int,
+				line_count: int) -> Dictionary:
+	while current < tokens.size():
+		if tokens[current].type == _SYMBOLS.BACKSLASH:
+			current += 2
+			line_count += 1
+			continue
+
+		if tokens[current].type in stop_tokens:
+			break
+
+		current += 1
+
+	return {
+		"next_ind": current,
+		"line_count": line_count
+	}
+
 func _parse_literal(tokens: Array[AST.Token], current: int, line_count: int) -> Dictionary:
-	var current_token: AST.Token = tokens[current]
+	var current_token := tokens[current]
 	return {
 		"node": ScriptAST.LiteralNode.new(current_token.value),
 		"next_ind": current + 1,
@@ -14,23 +36,16 @@ func _parse_class_name(tokens: Array[AST.Token], current: int, line_count: int) 
 	var class_name_node := ScriptAST.ScriptPropertyNode.new(ScriptAST.NodeType.CLASS_NAME, line_count, null)
 	current += 1
 
-	while current < tokens.size():
-		var token: AST.Token = tokens[current]
+	var skip: Dictionary = _skip_until([_SYMBOLS.NEW_LINE, _SYMBOLS.NAME], tokens, current, line_count)
+	current = skip["next_ind"]
+	line_count = skip["line_count"]
 
-		if token.type == ScriptSymbolIndex.SymbolType.NEW_LINE:
-			break
+	var token := tokens[current]
 
-		if token.type == ScriptSymbolIndex.SymbolType.BACKSLASH:
-			current += 2
-			line_count += 1
-			continue
+	if token.type == _SYMBOLS.NAME:
+		class_name_node.identifier = ScriptAST.IdentifierNode.new(token.value)
 
-		if token.type == ScriptSymbolIndex.SymbolType.NAME:
-			class_name_node.identifier = ScriptAST.IdentifierNode.new(token.value)
-			current += 1
-			break
-
-		current += 1
+	current += 1
 
 	return {
 		"node": class_name_node,
@@ -42,23 +57,16 @@ func _parse_parent_class(tokens: Array[AST.Token], current: int, line_count: int
 	var extends_node := ScriptAST.ScriptPropertyNode.new(ScriptAST.NodeType.EXTENDS, line_count, null)
 	current += 1
 
-	while current < tokens.size():
-		var token: AST.Token = tokens[current]
+	var skip: Dictionary = _skip_until([_SYMBOLS.NEW_LINE, _SYMBOLS.NAME], tokens, current, line_count)
+	current = skip["next_ind"]
+	line_count = skip["line_count"]
 
-		if token.type == ScriptSymbolIndex.SymbolType.NEW_LINE:
-			break
+	var token := tokens[current]
 
-		if token.type == ScriptSymbolIndex.SymbolType.BACKSLASH:
-			current += 2
-			line_count += 1
-			continue
+	if token.type == _SYMBOLS.NAME:
+		extends_node.identifier = ScriptAST.IdentifierNode.new(token.value)
 
-		if token.type == ScriptSymbolIndex.SymbolType.NAME:
-			extends_node.identifier = ScriptAST.IdentifierNode.new(token.value)
-			current += 1
-			break
-
-		current += 1
+	current += 1
 
 	return {
 		"node": extends_node,
@@ -69,32 +77,29 @@ func _parse_parent_class(tokens: Array[AST.Token], current: int, line_count: int
 func _parse_variable(tokens: Array[AST.Token], current: int, line_count: int) -> Dictionary:
 	var var_node := ScriptAST.VarDeclNode.new(line_count, null, null)
 	current += 1
-	while current < tokens.size():
-		var token: AST.Token = tokens[current]
 
-		if token.type == ScriptSymbolIndex.SymbolType.NEW_LINE:
-			break
+	var skip: Dictionary = _skip_until([_SYMBOLS.NEW_LINE, _SYMBOLS.NAME], tokens, current, line_count)
+	current = skip["next_ind"]
+	line_count = skip["line_count"]
 
-		if token.type == ScriptSymbolIndex.SymbolType.BACKSLASH:
-			current += 2
-			line_count += 1
-			continue
+	var token := tokens[current]
 
-		if token.type == ScriptSymbolIndex.SymbolType.NAME:
-			var_node.identifier = ScriptAST.IdentifierNode.new(token.value)
-			current += 1
-			continue
-
-		if token.type == ScriptSymbolIndex.SymbolType.EQUAL and var_node.identifier and not var_node.expression:
-			current += 1
-			var parsed: Dictionary = _parse_expression(tokens, current, line_count, false)
-
-			var_node.expression = parsed.get("node")
-			line_count = parsed["line_count"]
-			current = parsed["next_ind"]
-			continue
-
+	if token.type == _SYMBOLS.NAME and not var_node.identifier:
+		var_node.identifier = ScriptAST.IdentifierNode.new(token.value)
 		current += 1
+
+	skip = _skip_until([_SYMBOLS.NEW_LINE, _SYMBOLS.EQUAL], tokens, current, line_count)
+	current = skip["next_ind"]
+	line_count = skip["line_count"]
+
+	token = tokens[current]
+	if token.type == _SYMBOLS.EQUAL and var_node.identifier:
+		current += 1
+		var parsed: Dictionary = _parse_expression(tokens, current, line_count, false)
+
+		var_node.expression = parsed.get("node")
+		line_count = parsed["line_count"]
+		current = parsed["next_ind"]
 
 	return {
 		"node": var_node,
@@ -103,34 +108,38 @@ func _parse_variable(tokens: Array[AST.Token], current: int, line_count: int) ->
 	}
 
 func _parse_primary(tokens: Array[AST.Token], current: int, line_count: int) -> Dictionary:
-	while current < tokens.size():
-		var token: AST.Token = tokens[current]
+	var skip: Dictionary = _skip_until([_SYMBOLS.NEW_LINE,
+										_SYMBOLS.NAME,
+										_SYMBOLS.SQUARED,
+										_SYMBOLS.CURLY,
+										_SYMBOLS.LITERAL], tokens, current, line_count)
+	current = skip["next_ind"]
+	line_count = skip["line_count"]
 
-		if token.type == ScriptSymbolIndex.SymbolType.NEW_LINE:
-			break
+	if current >= tokens.size():
+		return {
+			"node": null,
+			"next_ind": current,
+			"line_count": line_count
+		}
 
-		if token.type == ScriptSymbolIndex.SymbolType.BACKSLASH:
-			current += 2
-			line_count += 1
-			continue
+	var token := tokens[current]
 
-		if token.type == ScriptSymbolIndex.SymbolType.NAME:
-			return {
-				"node": ScriptAST.IdentifierNode.new(tokens[current].value),
-				"next_ind": current + 1,
-				"line_count": line_count
-			}
+	if token.type == _SYMBOLS.NAME:
+		return {
+			"node": ScriptAST.IdentifierNode.new(token.value),
+			"next_ind": current + 1,
+			"line_count": line_count
+		}
 
-		if token.type == ScriptSymbolIndex.SymbolType.SQUARED and token.value == "[":
-			return _parse_array(tokens, current, line_count)
+	if token.type == _SYMBOLS.SQUARED and token.value == "[":
+		return _parse_array(tokens, current, line_count)
 
-		if token.type == ScriptSymbolIndex.SymbolType.CURLY and token.value == "{":
-			return _parse_dictionary(tokens, current, line_count)
+	if token.type == _SYMBOLS.CURLY and token.value == "{":
+		return _parse_dictionary(tokens, current, line_count)
 
-		if token.type == ScriptSymbolIndex.SymbolType.LITERAL:
-			return _parse_literal(tokens, current, line_count)
-
-		current += 1
+	if token.type == _SYMBOLS.LITERAL:
+		return _parse_literal(tokens, current, line_count)
 
 	return {
 		"node": null,
@@ -143,31 +152,33 @@ func _parse_postfix_chain(tokens: Array[AST.Token], primary: Dictionary, line_co
 	var current: int = primary["next_ind"]
 
 	while current < tokens.size():
-		var token: AST.Token = tokens[current]
+		var skip: Dictionary = _skip_until([_SYMBOLS.NEW_LINE,
+											_SYMBOLS.DOT,
+											_SYMBOLS.PARENTHESIS,
+											_SYMBOLS.SQUARED], tokens, current, line_count)
+		current = skip["next_ind"]
+		line_count = skip["line_count"]
 
-		if token.type == ScriptSymbolIndex.SymbolType.NEW_LINE:
+		if current >= tokens.size():
 			break
 
-		if token.type == ScriptSymbolIndex.SymbolType.BACKSLASH:
-			current += 2
-			line_count += 1
-			continue
-
-		if token.type == ScriptSymbolIndex.SymbolType.DOT:
+		var token := tokens[current]
+	
+		if token.type == _SYMBOLS.DOT:
 			current += 1
 			var name: String = tokens[current].value
 			node = ScriptAST.MemberAcessNode.new(line_count, node, ScriptAST.IdentifierNode.new(name))
 			current += 1
 			continue
 
-		if token.type == ScriptSymbolIndex.SymbolType.PARENTHESIS and token.value == "(":
+		if token.type == _SYMBOLS.PARENTHESIS and token.value == "(":
 			var call_node: Dictionary = _parse_function_call(tokens, current, line_count, node)
 			node = call_node["node"]
 			current = call_node["next_ind"]
 			line_count = call_node["line_count"]
 			continue
 
-		if token.type == ScriptSymbolIndex.SymbolType.SQUARED and token.value == "[":
+		if token.type == _SYMBOLS.SQUARED and token.value == "[":
 			var index_node: Dictionary = _parse_index(tokens, current, line_count, node)
 			node = index_node["node"]
 			current = index_node["next_ind"]
@@ -186,21 +197,17 @@ func _parse_expression(tokens: Array[AST.Token], current: int, line_count: int, 
 	var primary: Dictionary = _parse_primary(tokens, current, line_count)
 	primary = _parse_postfix_chain(tokens, primary, primary["line_count"])
 
-	line_count = primary["line_count"]
-	current = primary["next_ind"]
+	if expects_assign:
+		var skip: Dictionary = _skip_until([_SYMBOLS.EQUAL], tokens, primary["next_ind"], primary["line_count"])
+		current = skip["next_ind"]
+		line_count = skip["line_count"]
 
-	while current < tokens.size() and expects_assign:
-		var token: AST.Token = tokens[current]
+		if current >= tokens.size():
+			return primary
 
-		if token.type == ScriptSymbolIndex.SymbolType.NEW_LINE:
-			break
-
-		if token.type == ScriptSymbolIndex.SymbolType.BACKSLASH:
-			current += 2
-			line_count += 1
-			continue
-
-		if token.type == ScriptSymbolIndex.SymbolType.EQUAL:
+		var token := tokens[current]
+	
+		if token.type == _SYMBOLS.EQUAL:
 			current += 1
 			var parsed: Dictionary = _parse_expression(tokens, current, line_count, false)
 			return {
@@ -209,8 +216,6 @@ func _parse_expression(tokens: Array[AST.Token], current: int, line_count: int, 
 				"line_count": parsed["line_count"]
 			}
 
-		current += 1
-
 	return primary
 
 func _parse_array(tokens: Array[AST.Token], current: int, line_count: int) -> Dictionary:
@@ -218,18 +223,18 @@ func _parse_array(tokens: Array[AST.Token], current: int, line_count: int) -> Di
 	current += 1
 
 	while current < tokens.size():
-		var token: AST.Token = tokens[current]
+		var token := tokens[current]
 
-		if token.type == ScriptSymbolIndex.SymbolType.NEW_LINE:
+		if token.type == _SYMBOLS.NEW_LINE:
 			line_count += 1
 			current += 1
 			continue
 
-		if token.type == ScriptSymbolIndex.SymbolType.SQUARED and token.value == "]":
+		if token.type == _SYMBOLS.SQUARED and token.value == "]":
 			current += 1
 			break
 
-		if token.type == ScriptSymbolIndex.SymbolType.COMMA:
+		if token.type == _SYMBOLS.COMMA:
 			current += 1
 			continue
 
@@ -249,21 +254,21 @@ func _parse_dictionary(tokens: Array[AST.Token], current: int, line_count: int) 
 	current += 1
 
 	while current < tokens.size():
-		var token: AST.Token = tokens[current]
+		var token := tokens[current]
 
-		if token.type == ScriptSymbolIndex.SymbolType.NEW_LINE:
+		if token.type == _SYMBOLS.NEW_LINE:
 			line_count += 1
 			current += 1
 			continue
 
-		if token.type == ScriptSymbolIndex.SymbolType.CURLY and token.value == "}":
+		if token.type == _SYMBOLS.CURLY and token.value == "}":
 			current += 1
 			break
 
-		if token.type == ScriptSymbolIndex.SymbolType.COMMA:
+		if token.type == _SYMBOLS.COMMA:
 			current += 1
 			continue
- 
+
 		var pair: Dictionary = _parse_dict_pair(tokens, current, line_count)
 		dictionary_node.value.append(pair["node"])
 		current = pair["next_ind"]
@@ -284,23 +289,18 @@ func _parse_dict_pair(tokens: Array[AST.Token], current: int, line_count: int) -
 	line_count = key_node["line_count"]
 
 	while current < tokens.size():
-		var token: AST.Token = tokens[current]
+		var token := tokens[current]
 
-		if token.type == ScriptSymbolIndex.SymbolType.NEW_LINE:
+		if token.type == _SYMBOLS.NEW_LINE:
 			line_count += 1
 			current += 1
 			continue
 
-		if token.type == ScriptSymbolIndex.SymbolType.BACKSLASH:
-			current += 2
-			line_count += 1
-			continue
-
-		if token.type == ScriptSymbolIndex.SymbolType.COLON:
+		if token.type == _SYMBOLS.COLON:
 			current += 1
 			break
 	
-		if token.type == ScriptSymbolIndex.SymbolType.COMMA:
+		if token.type == _SYMBOLS.COMMA:
 			return {
 				"node": dict_pair_node,
 				"next_ind": current,
@@ -323,14 +323,14 @@ func _parse_index(tokens: Array[AST.Token], current: int, line_count: int, node:
 	current += 1
 
 	while current < tokens.size():
-		var token: AST.Token = tokens[current]
+		var token := tokens[current]
 
-		if token.type == ScriptSymbolIndex.SymbolType.NEW_LINE:
+		if token.type == _SYMBOLS.NEW_LINE:
 			line_count += 1
 			current += 1
 			continue
 
-		if token.type == ScriptSymbolIndex.SymbolType.SQUARED and token.value == "]":
+		if token.type == _SYMBOLS.SQUARED and token.value == "]":
 			current += 1
 			break
 
@@ -350,18 +350,18 @@ func _parse_function_call(tokens: Array[AST.Token], current: int, line_count: in
 	current += 1
 
 	while current < tokens.size():
-		var token: AST.Token = tokens[current]
+		var token := tokens[current]
 
-		if token.type == ScriptSymbolIndex.SymbolType.NEW_LINE:
+		if token.type == _SYMBOLS.NEW_LINE:
 			line_count += 1
 			current += 1
 			continue
 
-		if token.type == ScriptSymbolIndex.SymbolType.PARENTHESIS and token.value == ")":
+		if token.type == _SYMBOLS.PARENTHESIS and token.value == ")":
 			current += 1
 			break
 
-		if token.type == ScriptSymbolIndex.SymbolType.COMMA:
+		if token.type == _SYMBOLS.COMMA:
 			current += 1
 			continue
 
@@ -376,9 +376,31 @@ func _parse_function_call(tokens: Array[AST.Token], current: int, line_count: in
 		"line_count": line_count
 	}
 
-func _parse_function_decl(tokens: Array[AST.Token], current: int, line_count: int, tab_count: int) -> Dictionary:
+func _parse_return_value(tokens: Array[AST.Token], current: int, line_count: int) -> Dictionary:
+	var name := ScriptAST.IdentifierNode.new(tokens[current].value)
+	current += 1
+
+	var expression: Dictionary = _parse_expression(tokens, current, line_count, false)
+	return {
+		"node": ScriptAST.AssignmentNode.new(line_count, name, expression["node"]),
+		"next_ind": expression["next_ind"],
+		"line_count": expression["line_count"]
+	}
+
+func _parse_params(tokens: Array[AST.Token], current: int, line_count: int) -> Dictionary:
+	var params: Array[ScriptAST.IdentifierNode] = []
 	current += 1
 	return {
+		"node": params,
+		"next_ind": current,
+		"line_count": line_count
+	}
+
+func _parse_function_decl(tokens: Array[AST.Token], current: int, line_count: int, tab_count: int) -> Dictionary:
+	var func_decl_node := ScriptAST.FuncDeclNode.new(line_count, null, [], ScriptAST.ScopeNode.new(line_count, []))
+	current += 1
+	return {
+		"node": func_decl_node,
 		"next_ind": current,
 		"line_count": line_count
 	}
@@ -386,33 +408,35 @@ func _parse_function_decl(tokens: Array[AST.Token], current: int, line_count: in
 func _parse_conditional(tokens: Array[AST.Token], current: int, line_count: int, tab_count: int) -> Dictionary:
 	current += 1
 	return {
+		"node": ScriptAST.ScopeNode.new(line_count, []),
 		"next_ind": current,
 		"line_count": line_count
 	}
 
 func _parse_token(tokens: Array[AST.Token], current: int, line_count: int, tab_count: int) -> Dictionary:
-	var token: AST.Token = tokens[current]
+	var token := tokens[current]
+
 	match token.type:
-		ScriptSymbolIndex.SymbolType.CLASS_NAME: return _parse_class_name(tokens, current, line_count)
-		ScriptSymbolIndex.SymbolType.PARENT_CLASS: return _parse_parent_class(tokens, current, line_count)
-		ScriptSymbolIndex.SymbolType.VARIABLE: return _parse_variable(tokens, current, line_count)
-		ScriptSymbolIndex.SymbolType.FUNCTION: return _parse_function_decl(tokens, current, line_count, tab_count)
-		ScriptSymbolIndex.SymbolType.CONDITIONAL: return _parse_conditional(tokens, current, line_count, tab_count)
-		ScriptSymbolIndex.SymbolType.NAME: return _parse_expression(tokens, current, line_count, true)
-		ScriptSymbolIndex.SymbolType.RETURNS: return _parse_expression(tokens, current + 1, line_count, false)
-		ScriptSymbolIndex.SymbolType.NEW_LINE:
+		_SYMBOLS.CLASS_NAME: return _parse_class_name(tokens, current, line_count)
+		_SYMBOLS.PARENT_CLASS: return _parse_parent_class(tokens, current, line_count)
+		_SYMBOLS.VARIABLE: return _parse_variable(tokens, current, line_count)
+		_SYMBOLS.FUNCTION: return _parse_function_decl(tokens, current, line_count, tab_count)
+		_SYMBOLS.CONDITIONAL: return _parse_conditional(tokens, current, line_count, tab_count)
+		_SYMBOLS.NAME: return _parse_expression(tokens, current, line_count, true)
+		_SYMBOLS.RETURNS: return _parse_return_value(tokens, current, line_count)
+		_SYMBOLS.NEW_LINE:
 			return {
 				"next_ind": current + 1,
 				"line_count": line_count + 1,
 				"tab_count": 0
 			}
-		ScriptSymbolIndex.SymbolType.TABULATION:
+		_SYMBOLS.TABULATION:
 			return {
 				"next_ind": current + 1,
 				"line_count": line_count,
 				"tab_count": tab_count + 1
 			}
-		ScriptSymbolIndex.SymbolType.BACKSLASH:
+		_SYMBOLS.BACKSLASH:
 			return {
 				"next_ind": current + 2,
 				"line_count": line_count + 1,
--- addons/godot-graph-visualizer/project_scanners/script_scanner/script_reference_manager.gd
+++ addons/godot-graph-visualizer/project_scanners/script_scanner/script_reference_manager.gd
@@ -26,12 +26,59 @@ func find_script_with_path(path: String) -> ScriptData:
 
 	return null
 
+func print_child(child: AST.ASTNode) -> String:
+	if not child:
+		return "null"
+	var c: String = ""
+	if child is ScriptAST.VarDeclNode:
+		c = "%d:var %s = %s" % [child.line, print_child(child.identifier), print_child(child.expression)]
+	elif child is ScriptAST.IdentifierNode:
+		c = "%s" % child.name
+	elif child is ScriptAST.AssignmentNode:
+		c = "%d:%s = %s" % [child.line, print_child(child.target), print_child(child.expression)]
+	elif child is ScriptAST.CallNode:
+		c = "%s(" % print_child(child.callable)
+		for sub_c in child.args:
+			c += "%s, " % print_child(sub_c)
+		c += ")"
+	elif child is ScriptAST.MemberAcessNode:
+		c = "%s.%s" % [print_child(child.source), print_child(child.key)]
+	elif child is ScriptAST.ArrayNode:
+		c = "["
+		for sub_c in child.value:
+			c += "%s, " % print_child(sub_c)
+	elif child is ScriptAST.DictionaryNode:
+		c = "{"
+		for sub_c in child.value:
+			c += "%s, " % print_child(sub_c)
+		c += "}"
+	elif child is ScriptAST.DictPairNode:
+		c = "%s: %s" % [print_child(child.key), print_child(child.value)]
+	elif child is ScriptAST.LiteralNode:
+		c = str(child.value)
+	elif child is ScriptAST.ScriptPropertyNode:
+		c = "prop -> %s" % print_child(child.identifier)
+	elif child is ScriptAST.FuncDeclNode:
+		c = "func %s(" % print_child(child.identifier)
+		for sub_c in child.params:
+			c += "%s, " % print_child(sub_c)
+		c += ")"
+	else:
+		c = str(child.type)
+
+	return c
+
+func printAST(ast_root: ScriptAST.ScopeNode) -> void:
+	for child: AST.ASTNode in ast_root.body:
+		print(print_child(child))
+
 func build_all_references(script_paths: Array) -> void:
 	_scripts.clear()
 	var script_tokenizer: ScriptTokenizer = ScriptTokenizer.new()
 	var script_parser: ScriptParser = ScriptParser.new()
 	var script_property_builder: ScriptPropertyBuilder = ScriptPropertyBuilder.new()
-
+	
+	var i := 0
 	for path: String in script_paths:
 		var content: String = _get_script_content(path)
 		if content.is_empty():
@@ -39,8 +86,11 @@ func build_all_references(script_paths: Array) -> void:
 
 		var tokens: Array[AST.Token] = script_tokenizer.tokenize(content)
 		var ast_root: AST.ASTNode = script_parser.parse_script(tokens)
+		print(path)
+		printAST(ast_root)
 		var parsed_script: ScriptProperty = script_property_builder.build_property(ast_root)
 		_script_properties.add_script_to_database(path, parsed_script)
+		i+=1
 
 	var script_properties: Dictionary[String, ScriptProperty] = _script_properties.get_all_scripts()
 	var reference_builder: ReferenceBuilder = ReferenceBuilder.new(_script_properties)
